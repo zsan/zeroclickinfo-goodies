efe5a16f3d84a68c4ffdee10a11a751c2fecbde2
Jag Talon <talon.jag@gmail.com>
2012-10-05 23:05:27 -0400

    Added additional information about the phone number such as the city.

--- lib/DDG/Goodie/CallingCodes.pm
+++ lib/DDG/Goodie/CallingCodes.pm
@@ -2,21 +2,30 @@ package DDG::Goodie::CallingCodes;
 
 use DDG::Goodie;
 use Number::Phone;
-use Number::Phone::Country qw(noexport uk);
+use Number::Phone::Country;
 use Locale::Country;
 
 zci is_cached => 1;
 
 triggers query_lc => qr/
-                     \(?\+?\d+\)? #The first set of numbers.
+                     \(?\+\d+\)? #The first set of numbers.
                      ([\s\-\.]*\d+)* #The next set of numbers    
                      /ix;
 
 handle query_lc => sub {
+    my $output;
     if(my $iso_country_code = lc Number::Phone::Country::phone2country($_)) {
+        #Get the country code.
         $iso_country_code = $iso_country_code eq 'uk' ? 'gb' : $iso_country_code,
         my $country = Locale::Country::code2country($iso_country_code, 'alpha-2');
-        return $country;
+
+        #What else can we get from the phone number?
+        my $phone_info = Number::Phone->new($_);
+        my $human = $phone_info->format;
+        my $area = $phone_info->areaname;
+        $output = qq($human is a phone number in $country);
+        $output = qq($human is a phone number in $area, $country) if defined $area;
+        return $output;
     }
     return;
 };
