4991532b0c1622d5acd04c7c9f71609e27793c5f
Jag Talon <talon.jag@gmail.com>
2012-10-07 08:45:22 -0400

    Checks if the number is outside the NANP.

--- lib/DDG/Goodie/CallingCodes.pm
+++ lib/DDG/Goodie/CallingCodes.pm
@@ -8,7 +8,7 @@ use Locale::Country;
 zci is_cached => 1;
 
 triggers query_lc => qr/
-                     \(?\+\d+\)? #The first set of numbers.
+                     \(?\+?\d+\)? #The first set of numbers.
                      ([\s\-\.]*\d+)* #The next set of numbers    
                      /ix;
 
@@ -16,15 +16,20 @@ handle query_lc => sub {
     my $output;
     if(my $iso_country_code = lc Number::Phone::Country::phone2country($_)) {
         #Get the country code.
-        $iso_country_code = $iso_country_code eq 'uk' ? 'gb' : $iso_country_code,
+        $iso_country_code = $iso_country_code eq 'uk' ? 'gb' : $iso_country_code;
         my $country = Locale::Country::code2country($iso_country_code, 'alpha-2');
+        #Check if the number is not in the US.
+        return 'Numbers outside the North American Numbering Plan needs a leading + sign' if $iso_country_code eq 'nanp';
+        $output = qq($_ is a phone number in $country);
 
         #What else can we get from the phone number?
         my $phone_info = Number::Phone->new($_);
-        my $human = $phone_info->format;
-        my $area = $phone_info->areaname;
-        $output = qq($human is a phone number in $country);
-        $output = qq($human is a phone number in $area, $country) if defined $area;
+        if(defined $phone_info) {
+            my $human = $phone_info->format;
+            my $area = $phone_info->areaname;
+            $output = qq($human is a phone number in $country);
+            $output = qq($human is a phone number in $area, $country) if defined $area;
+        }
         return $output;
     }
     return;
