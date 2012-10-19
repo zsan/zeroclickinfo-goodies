8fef8ab1a0db3e303644ac9170678fc9c7a02dcf
Dylan Lloyd <dylan@dylansserver.com>
2012-10-19 15:20:53 -0400

    added "derive" to trigger

--- lib/DDG/Goodie/Differentiate.pm
+++ lib/DDG/Goodie/Differentiate.pm
@@ -3,14 +3,14 @@ package DDG::Goodie::Differentiate;
 use DDG::Goodie;
 use Math::Calculus::Differentiate;
 
-triggers startend => 'differentiate', 'd/dx of', 'derivative of';
+triggers startend => 'differentiate', 'derive', 'd/dx of', 'derivative of';
 
 zci answer_type => 'derivative';
 zci is_cached => 1;
 
 handle query_raw => sub {
     my $f = Math::Calculus::Differentiate->new;
-    s: ?(differentiate|derivative|d/dx)( of)? ?::g;
+    s: ?(differentiate|deriv(e|ative)|d/dx)( of)? ?::g;
     my ($var) = $_ =~ /([a-z])/;
     $f->addVariable($var);
     $f->setExpression($_);
